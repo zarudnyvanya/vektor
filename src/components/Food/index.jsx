e568a2cae99bd1f2eee89c48b41d75385f07778a
quro4ka <quro4ka@gmail.com>
2024-05-13 21:18:51 +0300

    fix: move to public

--- src/components/Food/index.jsx
+++ src/components/Food/index.jsx
@@ -1,243 +1,173 @@
 import styled from 'styled-components'
-import food1 from './../../assets/food/1.jpg'
-import food2 from './../../assets/food/2.jpg'
-import food3 from './../../assets/food/3.jpg'
-import food4 from './../../assets/food/4.jpg'
-import food5 from './../../assets/food/5.jpg'
-import food6 from './../../assets/food/6.jpg'
-import food7 from './../../assets/food/7.jpg'
-import food8 from './../../assets/food/8.jpg'
-import food9 from './../../assets/food/9.jpg'
-import food10 from './../../assets/food/10.jpg'
-import food11 from './../../assets/food/11.jpg'
-import food12 from './../../assets/food/12.jpg'
-import food13 from './../../assets/food/13.jpg'
-import food14 from './../../assets/food/14.jpg'
-import food15 from './../../assets/food/15.jpg'
-import food16 from './../../assets/food/16.jpg'
-import food17 from './../../assets/food/17.jpg'
-import food18 from './../../assets/food/18.jpg'
-import food19 from './../../assets/food/19.jpg'
-import food20 from './../../assets/food/20.jpg'
-import food21 from './../../assets/food/21.jpg'
-import food22 from './../../assets/food/22.jpg'
-import food23 from './../../assets/food/23.jpg'
-import food24 from './../../assets/food/24.jpg'
-import food25 from './../../assets/food/25.jpg'
-import food26 from './../../assets/food/26.jpg'
-import food27 from './../../assets/food/27.jpg'
-import food28 from './../../assets/food/28.jpg'
-import food29 from './../../assets/food/29.jpg'
-import food30 from './../../assets/food/30.jpg'
-import food31 from './../../assets/food/31.jpg'
-import food32 from './../../assets/food/32.jpg'
-import food33 from './../../assets/food/33.jpg'
-import food34 from './../../assets/food/34.jpg'
-import food35 from './../../assets/food/35.jpg'
-import food36 from './../../assets/food/36.jpg'
-import food37 from './../../assets/food/37.jpg'
-import food38 from './../../assets/food/38.jpg'
-import food39 from './../../assets/food/39.jpg'
-
-const foodList = [
-  {
-    name: 'eda',
-    img: food33,
-  },
-  {
-    name: 'eda',
-    img: food34,
-  },
-  {
-    name: 'eda',
-    img: food35,
-  },
-  {
-    name: 'eda',
-    img: food36,
-  },
-  {
-    name: 'eda',
-    img: food37,
-  },
-  {
-    name: 'eda',
-    img: food38,
-  },
-  {
-    name: 'eda',
-    img: food39,
-  },
-]
 
 const burgersList = [
   {
     name: 'гамбургер',
-    img: food4,
+    img: './4.jpg',
   },
   {
     name: 'гамбургер',
-    img: food5,
+    img: './5.jpg',
   },
   {
     name: 'гамбургер',
-    img: food6,
+    img: './6.jpg',
   },
   {
     name: 'гамбургер',
-    img: food7,
+    img: './7.jpg',
   },
   {
     name: 'гамбургер',
-    img: food8,
+    img: './8.jpg',
   },
   {
     name: 'гамбургер',
-    img: food9,
+    img: './9.jpg',
   },
 ]
 
 const pizzasList = [
   {
     name: 'eda',
-    img: food11,
+    img: './11.jpg',
   },
   {
     name: 'eda',
-    img: food12,
+    img: './12.jpg',
   },
   {
     name: 'eda',
-    img: food13,
+    img: './13.jpg',
   },
   {
     name: 'eda',
-    img: food14,
+    img: './14.jpg',
   },
   {
     name: 'eda',
-    img: food15,
+    img: './15.jpg',
   },
   {
     name: 'eda',
-    img: food16,
+    img: './16.jpg',
   },
   {
     name: 'eda',
-    img: food17,
+    img: './17.jpg',
   },
   {
     name: 'eda',
-    img: food18,
+    img: './18.jpg',
   },
   {
     name: 'eda',
-    img: food19,
+    img: './19.jpg',
   },
   {
     name: 'eda',
-    img: food20,
+    img: './20.jpg',
   },
   {
     name: 'eda',
-    img: food21,
+    img: './21.jpg',
   },
   {
     name: 'eda',
-    img: food22,
+    img: './22.jpg',
   },
   {
     name: 'eda',
-    img: food23,
+    img: './23.jpg',
   },
   {
     name: 'eda',
-    img: food24,
+    img: './24.jpg',
   },
   {
     name: 'eda',
-    img: food25,
+    img: './25.jpg',
   },
   {
     name: 'eda',
-    img: food26,
+    img: './26.jpg',
   },
   {
     name: 'eda',
-    img: food27,
+    img: './27.jpg',
   },
   {
     name: 'eda',
-    img: food28,
+    img: './28.jpg',
   },
 ]
 
 const varenikiList = [
   {
     name: 'eda',
-    img: food1,
+    img: './1.jpg',
   },
   {
     name: 'eda',
-    img: food2,
+    img: './2.jpg',
   },
   {
     name: 'eda',
-    img: food3,
+    img: './3.jpg',
   },
 ]
 
 const meatsList = [
   {
     name: 'eda',
-    img: food10,
+    img: './10.jpg',
   },
   {
     name: 'eda',
-    img: food33,
+    img: './33.jpg',
   },
   {
     name: 'eda',
-    img: food34,
+    img: './34.jpg',
   },
   {
     name: 'eda',
-    img: food35,
+    img: './35.jpg',
   },
   {
     name: 'eda',
-    img: food36,
+    img: './36.jpg',
   },
   {
     name: 'eda',
-    img: food37,
+    img: './37.jpg',
   },
   {
     name: 'eda',
-    img: food38,
+    img: './38.jpg',
   },
   {
     name: 'eda',
-    img: food39,
+    img: './39.jpg',
   },
 ]
 
 const oilList = [
   {
     name: 'eda',
-    img: food29,
+    img: './29.jpg',
   },
   {
     name: 'eda',
-    img: food30,
+    img: './30.jpg',
   },
   {
     name: 'eda',
-    img: food31,
+    img: './31.jpg',
   },
   {
     name: 'eda',
-    img: food32,
+    img: './32.jpg',
   },
 ]
 
